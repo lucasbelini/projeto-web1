b13f585676ae89daf7b8043d77a98a0448502cec
Belini <web01@powervending.com.br>
2022-06-28 18:30:52 -0300

    add view components

---
+++ view/componentes/header.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once($_SERVER['DOCUMENT_ROOT']."/projeto-web1/lib/const.lib.php");
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Metflix</title>
+    <link href="<?= _HTTP_LIB_PATH_ ?>css/bootstrap.css" rel="stylesheet">
+    <link href="<?= _HTTP_LIB_PATH_ ?>css/bootstrap-grid.css" rel="stylesheet">
+    <link href="<?= _HTTP_LIB_PATH_ ?>css/bootstrap-utilities.css" rel="stylesheet">
+</head>
+<body>
---
+++ view/login/index.php
@@ -0,0 +1,10 @@
+<?php
+    include(_ROOT_PATH_."view/componentes/header.php");
+?>
+
+    
+
+<?php
+    include(_ROOT_PATH_."view/componentes/scripts.php");
+    include(_ROOT_PATH_."view/componentes/ending.php");
+?>
